fbfc0dd56c5bbf60791cd0443a4212b48107580b
Funmi Ojo <oluwafunmi.ojo@gmail.com>
2019-03-13 13:02:25 -0400

    Removed only from maxSubArray test spec

--- tests/maxSubArray.spec.js
+++ tests/maxSubArray.spec.js
@@ -1,7 +1,7 @@
 const { expect } = require('chai')
 const maxSubArray = require('../maxSubArray')
 
-describe.only('maxSubArray', () => {
+describe('maxSubArray', () => {
   it('given an array of integers, finds the contiguous subarray with the largest sum and returns that sum', () => {
     expect(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4])).to.equal(6)
   })
